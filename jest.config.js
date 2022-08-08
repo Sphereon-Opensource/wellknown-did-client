987275d621df6f38aff06cc4389ba359f030e95a
nklomp <nklomp@sphereon.com>
2022-08-08 17:43:25 +0200

    chore: Ensure CI starts working

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
-  testRegex: '/test/.*\\.(test|spec)?\\.ts$',
+  rootDir: ".",
+  roots: ["<rootDir>/lib/", "<rootDir>/test/"],
+  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  // testRegex: '/test/.*\\.(test|spec)?\\.ts$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
